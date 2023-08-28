069044f27d84b128bb60771746cbdc799d3df91d
microgold <Batman134!!>
2023-08-28 12:31:45 -0500

    removed key from code

--- StoryTelling/StoryTellingOpenAI.py
+++ StoryTelling/StoryTellingOpenAI.py
@@ -1,24 +1,30 @@
 from io import BytesIO
+import os
 import tkinter as tk
 from tkinter import ttk
+from flask.cli import load_dotenv
 import openai
 import requests
 from PIL import Image, ImageTk
 
-openai.api_key = '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'
+load_dotenv()
+key = os.getenv("OPENAI_API_KEY")
+openai.api_key = key
+
 
 def generateImage(animal1, animal2, scenario):
-  response = openai.Image.create(
-    model="image-alpha-001",
-    prompt=f"cartoon image of a {animal1} and a {animal2} discussing {scenario}",
-    n=1,  # Number of images to generate
-    size="256x256",  # Size of the generated image
-    response_format="url"  # Format in which the image will be received
+    response = openai.Image.create(
+        model="image-alpha-001",
+        prompt=f"cartoon image of a {animal1} and a {animal2} discussing {scenario}",
+        n=1,  # Number of images to generate
+        size="256x256",  # Size of the generated image
+        response_format="url"  # Format in which the image will be received
     )
 
-  image_url = response.data[0]["url"]
-  print(image_url)
-  return image_url  
+    image_url = response.data[0]["url"]
+    print(image_url)
+    return image_url
+
 
 def update_label_with_new_image(label, photo):
     # Assuming `label` is a global variable
@@ -34,8 +40,8 @@ def display_image_from_url(image_holder, url):
 
     # Open and display the image using PIL and tkinter
     image = Image.open(image_data)
-    photo = ImageTk.PhotoImage(image)  
-    
+    photo = ImageTk.PhotoImage(image)
+
     update_label_with_new_image(image_holder, photo)
 
 
@@ -43,7 +49,7 @@ def submit():
     animal1 = combo1.get()
     animal2 = combo2.get()
     scenario = entry_box.get()
-    
+
     prompt = f"Create a play between a {animal1} and a {animal2} with 10 lines of dialog with each animal taking turns to speak. Leave a vertical space between lines after each animal speaks. Here is the scenario in which they will engage: {scenario}\n"
     # prompt = 'Hi There'
     messages = [{'role': 'user', 'content': prompt}]
@@ -54,8 +60,8 @@ def submit():
         top_p=1.0,
         frequency_penalty=0.0,
         presence_penalty=0.6,
-    )   
-    
+    )
+
     chatGPTAnswer = response["choices"][0]["message"]["content"]
     print(chatGPTAnswer)
     # make result_text scrollable
@@ -64,15 +70,11 @@ def submit():
     result_text.delete(1.0, tk.END)  # Clear any previous results
     result_text.insert(tk.END, chatGPTAnswer)
     result_text.config(state="disabled")
-      
+
     image_url = generateImage(animal1, animal2, scenario)
     display_image_from_url(image_holder, image_url)
 
-    
 
-
-
-    
 app = tk.Tk()
 app.title("Animal Discussion Scenario")
 
@@ -80,14 +82,16 @@ app.title("Animal Discussion Scenario")
 # Label and ComboBox for the first animal
 label1 = ttk.Label(app, text="Select Animal 1:")
 label1.grid(column=0, row=0, padx=10, pady=5)
-combo1 = ttk.Combobox(app, values=["Lion", "Elephant", "Giraffe", "Kangaroo", "Panda"])
+combo1 = ttk.Combobox(
+    app, values=["Lion", "Elephant", "Giraffe", "Kangaroo", "Panda"])
 combo1.grid(column=1, row=0, padx=10, pady=5)
 combo1.set("Lion")
 
 # Label and ComboBox for the second animal
 label2 = ttk.Label(app, text="Select Animal 2:")
 label2.grid(column=0, row=1, padx=10, pady=5)
-combo2 = ttk.Combobox(app, values=["Lion", "Elephant", "Giraffe", "Kangaroo", "Panda"])
+combo2 = ttk.Combobox(
+    app, values=["Lion", "Elephant", "Giraffe", "Kangaroo", "Panda"])
 combo2.grid(column=1, row=1, padx=10, pady=5)
 combo2.set("Elephant")
 
@@ -107,7 +111,8 @@ scrollbar = tk.Scrollbar(app)
 scrollbar.grid(row=4, column=3, sticky='ns')
 
 # Text widget to display results
-result_text = tk.Text(app, width=50, height=10, wrap=tk.WORD, yscrollcommand=scrollbar.set)
+result_text = tk.Text(app, width=50, height=10,
+                      wrap=tk.WORD, yscrollcommand=scrollbar.set)
 result_text.grid(column=0, row=4, columnspan=2, padx=10, pady=10)
 result_text.config(state="disabled")
 # result_text.pack(expand=True, fill=tk.BOTH)
@@ -119,9 +124,7 @@ image_holder.grid(column=0, row=5, columnspan=4, padx=10, pady=10)
 scrollbar.config(command=result_text.yview)
 
 
-
 # Link the scrollbar to the text widget (so the scrollbar knows how to scroll the text widget)
-
 
 
 app.mainloop()
